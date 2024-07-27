8c5826319f58b6a98e199748be14c8d24e987446
SamMoosavi <hi.sammoosavi@gmail.com>
2024-07-28 00:52:56 +0330

    feat(Home app): createed home app and virst view.

--- configs/urls.py
+++ configs/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Home.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home)
 ]
