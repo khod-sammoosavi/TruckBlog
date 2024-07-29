3beef50641abc109524eccad49b8ee114becdea5
SamMoosavi <hi.sammoosavi@gmail.com>
2024-07-29 10:44:56 +0330

    feat(tailwindcss): setup tailwindcss in project.

--- configs/urls.py
+++ configs/urls.py
@@ -18,7 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from Home.views import home
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home)
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
